8c823b2164ce0a8a56a0af4a67587c316c42b30a
dev22 <ysoli36512@gmail.com>
2023-09-15 10:26:20 +0900

    Step2 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -19,7 +19,9 @@ public class CustomWebApplicationServer {
     public CustomWebApplicationServer(int port) {
         this.port = port;
     }
-
+    /**
+     * Step2 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
+     */
     public void start() throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             logger.info("[CustomWebApplicationServer] started {} port.", port);
@@ -30,33 +32,9 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
-                /**
-                 * Step1 사용자 요청을 메인 Thread가 처리하도록 한다.
-                 */
-                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                            QueryStrings queryStrings = httpRequest.getQueryString();
-
-                            int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                            String operator = queryStrings.getValue("operator");
-                            int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                            int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                            byte[] body = String.valueOf(result).getBytes();
-
-                            HttpResponse response = new HttpResponse(dos);
-                            response.response200Header("application/json",body.length);
-                            response.responseBody(body);
-
-                    }
-                    }
-                }
+                new Thread(new ClientRequestHandler(clientSocket)).start();
             }
         }
     }
+}
 
